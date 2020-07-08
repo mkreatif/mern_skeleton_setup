25e3385dd84e8598b28b827c8138aa39ee3c892b
Marlin Kreatif <marlina.kreatif@gmail.com>
2020-07-08 23:24:02 +0700

    adding list process in user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,35 @@
 import User from "../models/user.model";
 import _ from 'lodash';
 import errorHandler from './error.controller';
+
+const create = (req, res, next) => {
+    const user = new User(req.body);
+    user.save((err, result) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler.getErrorMessage
+            })
+        }
+        res.status(200).json({
+            message: "Successfully signed up!"
+        })
+    })
+}
+const list = (req, res) => {
+    User.find((err, users) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler.getErrorMessage(err)
+            })
+        }
+        res.json(users)
+    }).select('name email updated created');
+}
+const userByID = (req, rest, next, id) => { }
+const read = (req, res) => { }
+const update = (req, res, next) => { }
+const remove = (req, res, next) => { }
+
+export default {
+    create, list, userByID, read, remove, update
+};
