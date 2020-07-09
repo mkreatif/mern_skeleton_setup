186e2ad8ba374663a1da77e7cce139e6124cc383
Marlin Kreatif <marlina.kreatif@gmail.com>
2020-07-09 09:49:19 +0700

    update : adding crud in user module

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -25,10 +25,50 @@ const list = (req, res) => {
         res.json(users)
     }).select('name email updated created');
 }
-const userByID = (req, rest, next, id) => { }
-const read = (req, res) => { }
-const update = (req, res, next) => { }
-const remove = (req, res, next) => { }
+const userByID = (req, res, next, id) => {
+    User.findById(id).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "User not found"
+            })
+        }
+        req.profile = user;
+        next()
+    })
+}
+const read = (req, res) => {
+    req.profile.hased_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+}
+const update = (req, res, next) => {
+    let user = req.profile;
+    user = _.extend(user, req.body);
+    user.update = Date.now();
+    user.save((err) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler.getErrorMessage(err)
+            })
+        }
+        user.hased_password = undefined;
+        user.salt = undefined;
+        res.json(user);
+    })
+}
+const remove = (req, res, next) => {
+    let user = req.profile;
+    user.remove((err, deleteUser) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler.getErrorMessage(err)
+            })
+        }
+        deleteUser.hased_password = undefined;
+        deleteUser.salt = undefined;
+        res.json(deleteUser);
+    })
+}
 
 export default {
     create, list, userByID, read, remove, update
